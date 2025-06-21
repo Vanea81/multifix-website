23adf62858d451a4cb5c7384f3585dae513c1eaa
Vanea81 <hybridheating38@gmail.com>
2025-06-21 11:25:19 -0400

    Delete js directory

--- js/gallery.js
+++
@@ -1,9 +0,0 @@
-const gallery = document.getElementById("gallery");
-const imageCount = 12;
-
-for (let i = 1; i <= imageCount; i++) {
-  const img = document.createElement("img");
-  img.src = `images/photo${i}.jpg`;
-  img.alt = `Project photo ${i}`;
-  gallery.appendChild(img);
-}
